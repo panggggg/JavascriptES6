80f2e8b2aa00bdbea7a019e065816f6ffb2b7a37
panggggg <p.picczy@gmail.com>
2021-05-31 14:41:10 +0700

    const variable

---
+++ Variables/constant.js
@@ -0,0 +1,13 @@
+name = 'Pang';
+age = 21;
+
+console.log(name)
+console.log(age + 1)
+
+/////////////////////
+
+const name2 = "Pawornwan";
+const age2 = 21;
+
+console.log(name2)
+age2 = age2 + 1; // <- Error : can't change value because age2 is constant variable
