b3546de4365ed758d6d72cf7aee506836da79339
panggggg <p.picczy@gmail.com>
2021-05-31 16:39:11 +0700

    Add arrow function

---
+++ ArrowFunctions/arrowFunction.js
@@ -0,0 +1,11 @@
+// const getRandomNumber = function (limit) {
+//     let random = Math.random() * limit;
+//     return Math.ceil(random)
+// }
+// console.log(getRandomNumber(100));
+
+//Arrow Funtion
+const getRandomNumber = limit =>
+    Math.ceil(Math.random() * limit);
+
+console.log(getRandomNumber(100));
