78e9d8d3e90dcac043c623af8ac648118fd61915
panggggg <p.picczy@gmail.com>
2021-05-31 15:55:43 +0700

    Add block scope

---
+++ BlockScope/blockScope.js
@@ -0,0 +1,37 @@
+// function countToTen() {
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i);
+//     }
+//     console.log(i); //i = 11
+// }
+
+// countToTen();
+
+// //////////////////////////////////
+// //ประกาศตัวแปร let; จะเรียกใช้ได้แค่ภายใน {} ที่ประกาศตัวแปรไว้
+// function countToTen() {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(i);
+//     }
+//     console.log(i); //Error: i is not defined
+// }
+
+// countToTen();
+
+////////////////////////////////
+
+function countToTen() {
+    function multiply2(n) {
+        return n * 2;
+    }
+    for (let i = 1; i <= 10; i++) {
+        function multiply(n) {
+            return n * 5;
+        }
+        console.log(multiply(i)); //ส่งค่า i ไปที่ multiply() line 28
+    }
+
+    console.log(multiply2(10)); //ส่ง 10 ไปที่ multiply() line 24
+}
+
+countToTen();
