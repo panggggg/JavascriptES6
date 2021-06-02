a3156504f3682c5e24e6b2f63b5e44ef96e1f2c6
panggggg <p.picczy@gmail.com>
2021-06-02 15:33:19 +0700

    add class example

---
+++ Classes/classes2.js
@@ -0,0 +1,26 @@
+class Person {
+    constructor(name) {
+        this.setName(name);
+    }
+    getName() {
+        return this.name;
+    }
+    setName(newName) {
+        newName = newName.trim();
+        if (newName === "") {
+            throw 'The name cannot be empty';
+        }
+        this.name = newName;
+    }
+}
+
+const person = new Person('Jane Doe');
+console.log(person);
+
+person.setName('Jane Smith');
+console.log(person.getName());
+
+//Class Person has 2 methods -> getName(), setName()
+//getName(); return value of name
+//setName(); assing name value, setName() removes the space of the newName throw that message is newName is empty
+//constructor() calls setName()
