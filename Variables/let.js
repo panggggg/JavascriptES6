effb078344b58d95487d8e4dd1ae9de96480f42e
panggggg <p.picczy@gmail.com>
2021-06-02 11:37:31 +0700

    add example for let variable

--- Variables/let.js
+++ Variables/let.js
@@ -6,3 +6,14 @@ console.log(x) // x is 10
     console.log(x) // x is 2
 }
 console.log(x) // x is 10
+
+for (let i = 0; i < 5; i++) {
+    console.log("Inside the loop: ", i);
+    // Output: Inside the loop:  0
+    //         Inside the loop:  1
+    //         Inside the loop:  2
+    //         Inside the loop:  3
+    //         Inside the loop:  4
+}
+
+console.log("Outside the loop: ", i); //Output: Error
