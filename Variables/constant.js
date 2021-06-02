11e41ec5d6aa9ebbf0d1c0a0044c8f3febb428c1
panggggg <p.picczy@gmail.com>
2021-06-03 00:43:01 +0700

    edit

--- Variables/constant.js
+++ Variables/constant.js
@@ -1,34 +1,13 @@
-name = 'Pang';
-age = 21;
+//const is block-scpoed
+//can't change value
 
-console.log(name)
-console.log(age + 1)
+let a = 0
+a = 1
 
-//////////////////////////////////////
+const PI = 3.14
+// PI = 1 -> Error 
 
-const name2 = "Pawornwan";
-const age2 = 21;
-
-console.log(name2)
-
-//age2 = age2 + 1; // <- Error : can't change value because age2 is constant variable
-
-//////////////////////////////////////
-
-const array = [1, 2, 3, 4, 5];
-const obj = {
-    name: "Pang",
-    age: 21
-};
-
-console.log(array);
-console.log(obj);
-
-//การเพิ่มค่าใน array
-//array = [6, 7, 8, 9, 10] <- Error : because array is constant variable can't change the value.
-array.push(6)
-console.log(array);
-
-//เปลี่ยนค่า age ใน obj
-obj.age = 22;
-console.log(obj);
+const obj = { a: 1 }
+//การเปลี่ยนค่าภายใน object ไม่ได้ทำให้ memory address เปลี่ยนไป
+obj.a = 2
+console.log(obj) //Output: {a:2}
