ea9601b7ed968c514bda2a99edba75eefaa1b22a
panggggg <p.picczy@gmail.com>
2021-06-02 17:42:53 +0700

    add Array.of() example

---
+++ Array/arrayOf.js
@@ -0,0 +1,7 @@
+const numbers = Array.of(3);
+console.log(numbers.length); //1
+console.log(numbers[0]); //3
+
+const chars = Array.of('A', 'B', 'C');
+console.log(chars.length); //3
+console.log(chars);  //[ 'A', 'B', 'C' ]
